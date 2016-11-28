4180ebc0c754f9739e783ac1446d3ca7e61cd69d
leesanghyeok <1004xlsrn@gmail.com>
2016-11-29 01:02:44 +0900

    SSU공지사항 가져오기. 학교 공지사항이 XML로 보여주지 않고 HTML로 보여줘서 Response를 FIle로 변경해서 Rome로 파싱하도록 했다. 순수 JAVA에서는 문제없이 돌아가는데, android에서는 Text가 잘린다. 내부 저장소에 파일 용량에 관한 문제라고 추측한다.

--- app/src/main/java/com/notissu/SyncAdapter/SyncAdapter.java
+++ app/src/main/java/com/notissu/SyncAdapter/SyncAdapter.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
 
+import com.notissu.Util.IOUtils;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -18,6 +19,7 @@ import com.rometools.rome.io.XmlReader;
 
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -31,17 +33,20 @@ import java.text.ParseException;
 
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
     private static final String TAG = SyncAdapter.class.getName();
-    private static final String FEED_URL = "https://leesanghyeok.github.io/feed.xml"; //내 블로그 임시
-
+//    private static final String FEED_URL = "https://leesanghyeok.github.io/feed.xml"; //내 블로그 임시
+    private static final String FEED_URL = "http://www.ssu.ac.kr/web/kor/plaza_d_01;jsessionid=yIPyJDhVJSyGG1SWk3kZeQ5qXfdbVfqihsikvlZZVAILUn5tgH2HjcX4fiQFXD40?p_p_id=EXT_MIRRORBBS&p_p_lifecycle=0&p_p_state=exclusive&p_p_mode=view&p_p_col_id=column-1&p_p_col_pos=1&p_p_col_count=2&_EXT_MIRRORBBS_struts_action=%2Fext%2Fmirrorbbs%2Frss"; //내 블로그 임시
+    Context context;
     ContentResolver mContentResolver;
 
     public SyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
+        this.context = context;
         mContentResolver = context.getContentResolver();
     }
 
     public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
+        this.context = context;
         mContentResolver = context.getContentResolver();
     }
 
@@ -53,6 +58,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
             Log.i(TAG, "Streaming data from network: " + location);
             updateLocalFeedData(location, syncResult);
+
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
         } catch (MalformedURLException e) {
@@ -89,9 +95,13 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     private void updateLocalFeedData(final URL feedUrl, final SyncResult syncResult)
             throws IOException, XmlPullParserException, RemoteException,
             OperationApplicationException, ParseException, FeedException{
-        Log.d(TAG,"helloworld");
+
+        File file = IOUtils.URLToFile(context, FEED_URL);
+
+        Log.d(TAG,IOUtils.fileToString(file));
+
         SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(feedUrl));
+        SyndFeed feed = input.build(new XmlReader(file));
 
         Log.d(TAG,feed+"");
     }
