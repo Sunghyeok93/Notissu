135d3fed0fb9dfd99f4ac963a1ba817abc927e2f
dnfldml3476 <wpswkdgozld@naver.com>
2016-11-28 17:24:25 +0900

    keyword 입력시 메뉴창에 키워드 생성

--- app/src/main/java/com/notissu/Dialog/AddKeywordDialog.java
+++ app/src/main/java/com/notissu/Dialog/AddKeywordDialog.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.notissu.R;
 
@@ -14,8 +15,12 @@ import com.notissu.R;
  * Created by Sunghyeok on 2016-11-03.
  */
 
-public class AddKeywordDialog extends DialogFragment implements View.OnClickListener{
-    Button keyword_button;
+public class AddKeywordDialog extends DialogFragment {
+    View rootView;
+    Button btnKeyword;
+    EditText etText;
+
+    OnAddKeywordListner onAddKeywordListner;
 
     public static AddKeywordDialog newInstance() {
         Bundle args = new Bundle();
@@ -31,17 +36,47 @@ public class AddKeywordDialog extends DialogFragment implements View.OnClickList
 
         getDialog().setTitle("키워드 추가");
 
-        View view = inflater.inflate(R.layout.dialog_keyword, null, false);
+        rootView = inflater.inflate(R.layout.dialog_keyword, null, false);
 
-        keyword_button = (Button) view.findViewById(R.id.button_keyword_set);
+        btnKeyword = (Button) rootView.findViewById(R.id.button_keyword_set);
+        etText = (EditText) rootView.findViewById(R.id.edittext_keyword_set);
 
-        keyword_button.setOnClickListener(this);
 
-        return view;
+        btnKeyword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                String output = etText.getText().toString();
+                bundle.putString("aa",output);
+                onAddKeywordListner.onAdd(bundle);
+                dismiss();
+                /*Intent intent = new Intent();
+                ComponentName componentName = new ComponentName(
+                        "com.notissu",
+                        "com.notissu.Activity.MainActivity");
+                intent.setComponent(componentName);
+
+
+                String output = etText.getText().toString();
+                intent.putExtra("Keyword_name", output);
+                startActivity(intent);
+
+*/
+            }
+        });
+     //   btnKeyword.setOnClickListener(this);
+
+
+
+        return rootView;
+
     }
 
-    @Override
-    public void onClick(View view) {
+    public void setOnAddKeywordListner(OnAddKeywordListner onAddKeywordListner) {
+        this.onAddKeywordListner = onAddKeywordListner;
+    }
 
+    public interface OnAddKeywordListner {
+        void onAdd(Bundle bundle);
     }
 }
