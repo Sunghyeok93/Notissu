511b3b0f619df98415a6abb7e3d685444e549c45
Sung\Sunghyeok <secondray1234@gmail.com>
2016-12-04 20:04:47 +0900

    option fragment제작

--- app/src/main/java/com/notissu/Activity/MainActivity.java
+++ app/src/main/java/com/notissu/Activity/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,12 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 
 import com.notissu.Dialog.AddKeywordDialog;
 import com.notissu.Fragment.NoticeListFragment;
 import com.notissu.Fragment.NoticeTabFragment;
+import com.notissu.Fragment.OptionFragment;
 import com.notissu.Model.NoticeRow;
 import com.notissu.R;
 import com.notissu.Util.ResString;
@@ -185,6 +184,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_starred) {
             ArrayList<NoticeRow> noticeRows = Str.STARRED_SSU_NOTICES;
             fragmentTransaction.replace(R.id.main_fragment_container,NoticeListFragment.newInstance(noticeRows)).commit();
+        } else if(id == R.id.nav_option)
+        {
+            fragmentTransaction.replace(R.id.main_fragment_container,new OptionFragment()).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
