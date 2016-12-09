32b935df7b07863b157a0f76d666ba34a680fa59
Sung\Sunghyeok <secondray1234@gmail.com>
2016-12-09 19:47:25 +0900

    설정창 구현했고 이제 각 내용들 구현하려함

--- app/src/main/java/com/notissu/Fragment/OptionFragment.java
+++ app/src/main/java/com/notissu/Fragment/OptionFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.notissu.R;
 
@@ -14,12 +18,36 @@ import com.notissu.R;
 
 public class OptionFragment extends Fragment{
     private static final String KEY_STATE_BUNDLE = "OptionFragment";
+    static final String[] LIST_MENU = {"업데이트 주기 설정", "키워드 설정 편집", "ㅎㅎ"} ;
     View rootView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_option,container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_setting,container, false);
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) rootView.findViewById(R.id.setting_list) ;
+        listview.setDividerHeight(2);
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                String strText = (String) parent.getItemAtPosition(position) ;
+                switch (strText){
+                    case "업데이트 주기 설정" : Toast.makeText(getContext(), "111", Toast.LENGTH_LONG).show();
+                    case "키워드 설정 편집" : Toast.makeText(getContext(), "222", Toast.LENGTH_LONG).show();
+                    case "List 3" : Toast.makeText(getContext(), "미구현", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }) ;
+
         return rootView;
     }
+
 }
 
