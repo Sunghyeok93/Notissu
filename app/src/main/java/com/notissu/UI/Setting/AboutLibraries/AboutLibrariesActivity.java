a6137322ac6c5293a3c9f41ac08f4cda845f3c37
leesanghyeok <1004xlsrn@gmail.com>
2017-02-11 17:18:03 +0900

    ResString 제거

--- app/src/main/java/com/notissu/UI/Setting/AboutLibraries/AboutLibrariesActivity.java
+++ app/src/main/java/com/notissu/UI/Setting/AboutLibraries/AboutLibrariesActivity.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.LibsBuilder;
 import com.mikepenz.aboutlibraries.ui.LibsActivity;
-import com.notissu.Util.ResString;
+import com.notissu.R;
+
+import butterknife.BindString;
+import butterknife.ButterKnife;
 
 public class AboutLibrariesActivity extends LibsActivity {
+    @BindString(R.string.setting_title_about_libraries)
+    String mTitle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
         LibsBuilder libsBuilder = new LibsBuilder()
-                .withLibraries("numberpicker","rome")
-                .withActivityTitle(ResString.getInstance().getString(ResString.RES_SETTING_TITLE_ABOUT_LIBRARIES))
+                .withLibraries("numberpicker", "rome")
+                .withActivityTitle(mTitle)
                 .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
                 .withActivityTheme(com.mikepenz.aboutlibraries.R.style.AboutLibrariesTheme_Light_DarkToolbar);
 
